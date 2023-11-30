e94e0baa0dec1eae73545f165cf88426929f6901
Shota-Kizaki <104074879+Shota-Kizaki@users.noreply.github.com>
2023-12-01 00:53:41 +0900

    いちおう

--- home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-#from login.views import LogoutView
-urlpatterns = [
-    path("home/", views.home, name="home"),
-    #path('logout/', LogoutView.as_view(template_name='login/logout.html'), name='logout'),
-]
